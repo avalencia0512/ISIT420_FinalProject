39db73eac57c4699b20df69b7581ed58f3f61993
Terry Consulting <truptidalia@gmail.com>
2021-06-22 23:18:36 -0700

    All works Perfect

--- HealthyWealthyApp/HealthyWealthyApp/Models/HappyIncome.cs
+++ HealthyWealthyApp/HealthyWealthyApp/Models/HappyIncome.cs
@@ -1,4 +1,6 @@
-﻿namespace HealthyWealthyApp.Models
+﻿using System;
+
+namespace HealthyWealthyApp.Models
 {
     public class HappyIncome
     {
@@ -9,5 +11,18 @@
 
         public double AvgWealthPP { get; set; }
 
+        public override bool Equals(object obj)
+        {
+            if (!(obj is HappyIncome))
+                return false;
+            HappyIncome p = (HappyIncome)obj;
+            return (p.CountryId == CountryId && p.CountryName == CountryName && p.AvgHappyScore == AvgHappyScore && 
+                p.AvgWealthPP == AvgWealthPP);
+        }
+
+        public override int GetHashCode()
+        {
+            return String.Format("{0}|{1}|{2}|{3}", CountryId, CountryName, AvgHappyScore, AvgWealthPP).GetHashCode();
+        }
     }
 }
--- HealthyWealthyApp/HealthyWealthyApp/Models/HappyLife.cs
+++ HealthyWealthyApp/HealthyWealthyApp/Models/HappyLife.cs
@@ -1,4 +1,6 @@
-﻿namespace HealthyWealthyApp.Models
+﻿using System;
+
+namespace HealthyWealthyApp.Models
 {
     public class HappyLife
     {
@@ -8,6 +10,18 @@
         public double AvgHappyScore { get; set; }
 
         public double AvgLifeSpan { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is HappyLife))
+                return false;
+            HappyLife p = (HappyLife)obj;
+            return (p.CountryId ==CountryId && p.CountryName == CountryName && p.AvgHappyScore == AvgHappyScore && p.AvgLifeSpan == AvgLifeSpan);
+        }
+        public override int GetHashCode()
+        {
+            return String.Format("{0}|{1}|{2}|{3}", CountryId, CountryName, AvgHappyScore, AvgLifeSpan).GetHashCode();
+        }
 
     }
 }
